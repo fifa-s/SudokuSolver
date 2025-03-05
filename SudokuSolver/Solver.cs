5b59c3b0ae571e372529412cd55a25dd167b2d9a
Filip Svoboda <svoboda.11filip@gmail.com>
2025-03-05 20:56:46 +0100

    board checking

--- SudokuSolver/Solver.cs
+++ SudokuSolver/Solver.cs
@@ -17,6 +17,9 @@ namespace SudokuSolver
         {
             this.SudokuControl.ReadOnly = true;
 
+            if (!CheckBoard())
+                return;
+
             List<int> solutionIndexes = new List<int>();
 
             for (int j = 0; j < 81; j++)
@@ -73,9 +76,15 @@ namespace SudokuSolver
             }
         }
 
+
+        // Same as Solve(), but animated
         public void SolveAnimate()
         {
             this.SudokuControl.ReadOnly = true;
+
+
+            if (!CheckBoard())
+                return;
 
             List<int> solutionIndexes = new List<int>();
 
@@ -133,6 +142,53 @@ namespace SudokuSolver
                 Thread.Sleep(1);
                 Application.DoEvents();
             }
+        }
+
+        // Returns true if board is valid
+        private bool CheckBoard()
+        {
+            int[] board = new int[81];
+
+            for (int i = 0; i < 81; i++)
+            {
+                board[i] = this.SudokuControl.GetAt(i);
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                bool rowDuplicates = board
+                    .Skip(i * 9)
+                    .Take(9)
+                    .GroupBy(x => x)
+                    .Where(g => g.Count() > 1 && g.Key != 0)
+                    .Any();
+
+                bool colDuplicates = Enumerable.Range(0, 9)
+                    .Select(row => board[i + 9 * row])
+                    .GroupBy(x => x)
+                    .Where(g => g.Count() > 1 && g.Key != 0)
+                    .Any();
+
+                bool squareDuplicates = Enumerable.Range(0, 9)
+                    .Select(j => {
+                        int squareX = (i % 3) * 3;
+                        int squareY = (i / 3) * 3;
+                        int x = squareX + j % 3;
+                        int y = squareY + j / 3;
+                        return board[x + y * 9];
+                    })
+                    .GroupBy(x => x)
+                    .Where(g => g.Count() > 1 && g.Key != 0)
+                    .Any();
+
+                if (rowDuplicates || colDuplicates || squareDuplicates)
+                {
+                    MessageBox.Show($"This board is not valid.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         private List<int> GetPossibleNumbers(int index)
